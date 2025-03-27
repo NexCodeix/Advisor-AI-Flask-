43fd8d44efb75841daf417bcd225c0d0a8cc8368
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2025-03-28 00:17:28 +0300

    remove socketio

--- main/consumers.py
+++ main/consumers.py
@@ -1,7 +1,6 @@
 import json
 import time
 import threading
-import socketio
 import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
